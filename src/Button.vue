8706ef60bfd4c1e48a008c3f4397eaa1d2a2c227
Liqiaomiao <1049259465@qq.com>
2018-06-27 09:05:23 +0800

    button变成单文件

---
+++ src/Button.vue
@@ -0,0 +1,32 @@
+<style lang="scss">
+    .g-button{
+        height:var(--button-height);
+        font:inherit;
+        border-radius: var(--border-radius);
+        border:1px solid var(--border-color);
+        background: var(--button-bg);
+        padding: 0 1em;
+        &:hover{
+            border-color: var(--border-color-hover);
+         }
+        &:active{
+             background: var(--button-active-bg);
+        }
+        &:focus{
+             outline: none;
+        }
+    }
+</style>
+<template>
+    <div>
+        <button class="g-button">按钮</button>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return {}
+        }
+    }
+</script>
--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
  * Created by Liqiaomiao on 2018/6/26.
  */
 import Vue from 'vue'
-import Button from './button'
+import Button from './button.vue'
 
 new Vue({
     el:"#app"
