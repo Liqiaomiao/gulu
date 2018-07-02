b1d6aeb8c164b6b836146f6f363cbfc97815e9eb
Liqiaomiao <1049259456@qq.com>
2018-07-03 00:50:01 +0800

    增加属性的检查器

--- src/Button.vue
+++ src/Button.vue
@@ -61,7 +61,14 @@
             icon:{},
             iconPosition:{
                 type:String,
-                default:'left'
+                default:'left',
+                validator(value){
+                    if(value!='left'&&value!='right' ){
+                        return false
+                    }else{
+                        return true
+                    }
+                }
             }
         }
     }
