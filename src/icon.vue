b070f8b171217caaefbbfad128f3a5b085039e2f
Liqiaomiao <1049259465@qq.com>
2018-07-03 09:27:43 +0800

    将svg代码整理到src/icon.vue

---
+++ src/icon.vue
@@ -0,0 +1,19 @@
+<style lang="scss">
+    .icon {
+        height: 1em;
+        width: 1em;
+    }
+</style>
+<template>
+    <div>
+        <svg v-if="name" class="icon">
+            <use :xlink:href="`#i-${name}`"></use>
+        </svg>
+    </div>
+</template>
+
+<script>
+    export default{
+        props:['name']
+    }
+</script>
