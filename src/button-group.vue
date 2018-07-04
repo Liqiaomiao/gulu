3c7ae2654cb655702f57b28826d49bb042472837
Liqiaomiao <1049259465@qq.com>
2018-07-04 19:29:09 +0800

    增加'button-group'

---
+++ src/button-group.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="g-button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return {}
+        }
+    }
+</script>
+<style lang="scss">
+
+
+</style>
