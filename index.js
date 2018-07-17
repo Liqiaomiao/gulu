bb3db95b3b7077fb80738df61c478ea304428d63
Liqiaomiao <1049259456@qq.com>
2018-07-17 23:36:06 +0800

    增加入口文件；更新package.json

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Liqiaomiao on 2018/7/17.
+ */
+import Button from './src/Button'
+import ButtonGroup from './button-group.vue'
+import Icon from './icon.vue'
+export {Button,ButtonGroup,Icon}
