4a8b1d79ee646048e4f6c4b8c0f000836e2c6510
Liqiaomiao <1049259456@qq.com>
2018-07-24 10:23:44 +0800

    更新readme

--- src/Input.vue
+++ src/Input.vue
@@ -1,15 +1,42 @@
-<template>
-    <div>
-
-    </div>
-</template>
 <style>
 
 </style>
+<template>
+    <div >
+        <input type="text"
+               :value="value"
+               :disabled="disabled"
+               :readonly="readonly"
+
+        />
+        <Icon v-if="error"  name="error"></Icon>
+        <i  v-if="error" class='errorMessage'>{{error}}</i>
+    </div>
+</template>
+
 <script>
+    import Icon from  'icon'
     export default{
         data(){
             return {}
+        },
+        components:{
+            Icon
+        },
+        props: {
+            value: {},
+            disabled: {
+                type: Boolean,
+                default: false
+            },
+            readonly: {
+                type: Boolean,
+                default: false
+            },
+            error:{
+
+            },
+
         }
     }
 </script>
