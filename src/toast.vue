103ce98b3b169bcb46b61e4baff2d0265e37cfcc
Liqiaomiao <1049259456@qq.com>
2018-09-02 14:50:48 +0800

    创建popover组件

--- src/toast.vue
+++ src/toast.vue
@@ -50,12 +50,11 @@
                     [`position-${this.position}`]:true
                 }
             }
+
         },
         mounted(){
             this.execAutoClose();
             this.updateStyles();
-
-
         },
         methods:{
             updateStyles(){
@@ -96,26 +95,50 @@
     $font-size:14px;
     $min-height:40px;
     $toast-bg:rgba(0,0,0,.75);
-    @keyframes fadeIn {
+    $duration:1s;
+    @keyframes slideUp {
         0%{opacity: 0;transform: translateY(100%)}
         100%{opacity:1;transform: translateY(0%)}
     }
+    @keyframes slideDown {
+        0%{opacity: 0;transform: translateY(-100%)}
+        100%{opacity:1;transform: translateY(0%)}
+    }
+    @keyframes fadeIn {
+        0%{opacity: 0;}
+        100%{opacity:1}
+    }
+
     .wrapper{
         position: fixed;
         left: 50%;
         transform:translateX(-50%);
         &.position-top{
-             top:0
+             top:0;
+            &> .toast{
+                   animation:slideDown $duration;
+                   border-top-left-radius: 0;
+                   border-top-right-radius: 0;
+               }
+
+
          }
         &.position-bottom{
              bottom:0;
+            &> .toast{
+                   animation:slideUp $duration;
+                   border-bottom-left-radius: 0;
+                   border-bottom-right-radius: 0;
+               }
+
          }
         &.position-middle{
+             animation:fadeIn $duration;
              top:50%;
              transform: translate(-50%,-50%);
          }
         .toast{
-            animation:fadeIn 1s;
+
             font-size: $font-size;
             line-height: 1.8;
             min-height:  $min-height;
