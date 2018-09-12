87a3d6f550685bcd1761134da183845d0dff3265
Liqiaomiao <1049259456@qq.com>
2018-09-12 23:08:11 +0800

    'cascader'递归渲染左右结构

--- src/cascader-item.vue
+++ src/cascader-item.vue
@@ -1,17 +1,20 @@
 <template>
     <div class="cascaderItem">
-        {{sourceItem.name}}
-        <cascader-item
-                v-if="sourceItem.children"
-                v-for="(item,index) in sourceItem.children"
-                :sourceItem="item"
-        ></cascader-item>
+        <div class="left" >
+            <div class="label" v-for="(items,index) in sourceItem" @click="leftSelected=items">
+                {{items.name}}
+            </div>
+        </div>
+        <div class="right" v-if="rightItems&&rightItems.length>0"  >
+            <cascader-item :sourceItem="rightItems">
+            </cascader-item>
+        </div>
     </div>
 </template>
 <style>
 .cascaderItem{
     border: 1px solid red;
-    margin: 10px;
+  display: flex;
 }
 </style>
 <script>
@@ -19,11 +22,23 @@
         name:'cascaderItem',
         props:{
             sourceItem:{
-                type:Object
+                type:Array
             }
         },
         data(){
-            return {}
+            return {
+                leftSelected:null,
+            }
+        },
+        computed:{
+            rightItems(){
+                if(this.leftSelected){
+                    return this.leftSelected.children
+                }else{
+                    return []
+                }
+
+            }
         }
     }
 </script>
--- src/cascader.vue
+++ src/cascader.vue
@@ -3,21 +3,9 @@
         <div class="trigger" @click="popoverVisible=!popoverVisible">
         </div>
         <div class="popover" v-if="popoverVisible">
-            <div class="level1" >
-                <div class="label" v-for="(item1,index) in source" @click="level1Selected=item1">
-                    {{item1.name}}
-                </div>
-            </div>
-            <div class="level2" >
-                <div class="label" v-for="(item2,index) in level2Items" @click="level2Selected=item2">
-                    {{item2.name}}
-                </div>
-            </div>
-            <div class="level3" >
-                <div class="label" v-for="(item3,index) in level3Items">
-                    {{item3.name}}
-                </div>
-            </div>
+
+            <cascader-item  :sourceItem="source"></cascader-item>
+
         </div>
     </div>
 </template>
@@ -44,16 +32,8 @@
         computed:{
             level2Items(){
                 if(this.level1Selected){
-                    console.log(this.level1Selected);
-                    return this.level1Selected.children
-                }else{
-                    return []
-                }
 
-            },
-            level3Items(){
-                if(this.level2Selected){
-                    return this.level2Selected.children
+                    return this.level1Selected.children
                 }else{
                     return []
                 }
