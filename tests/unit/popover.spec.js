81ff7e16f8feb3169962ae3d6a576735293028ad
Liqiaomiao <1049259456@qq.com>
2018-12-17 00:10:46 +0800

    补充popover的trigger测试

--- tests/unit/popover.spec.js
+++ tests/unit/popover.spec.js
@@ -16,11 +16,29 @@ describe('popover.vue', () => {
             slots:{
                 default:'<button>点我</button>',
                 content:'<div>弹出内容</div>'
+            },
+            propsData:{
+                position:'bottom'
             }
         });
         wrapper.find('button').trigger('click');
         let classes = wrapper.find('.content-wrapper').classes();
-        expect(classes).to.include('content-wrapper')
+        expect(classes).to.include('position-bottom')
+    });
+    it('可以设置trigger',()=>{
+        const wrapper = mount(popover,{
+            slots:{
+                default:'<button>点我</button>',
+                content:'<div>弹出内容</div>'
+            },
+            propsData:{
+                position:'bottom',
+                trigger:'hover'
+            }
+        })
+        expect(wrapper.find('.content-wrapper').element).to.not.exist
+        wrapper.find('.popover').trigger('mouseenter');
+        expect(wrapper.find('.content-wrapper').element).to.exist
     })
 
 });
